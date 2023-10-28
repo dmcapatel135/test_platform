6e2c7b12917ac5fab2bfe1f738cdf325b1cd3f62
Dharmendra patel <dmcapatel135@gmail.com>
2023-10-28 19:50:19 +0530

    updated code

--- src/screens/institutescreen/InstLogin/InstLogin.js
+++ src/screens/institutescreen/InstLogin/InstLogin.js
@@ -5,10 +5,11 @@ import React from "react";
 const InstLogin = () => {
     return(
         <div className="container">
-            hii
+            hii my name is  dharmendra patel
             <div className="row m-0 p-0">
-               <div className="col-12">
+               <div className="col-12 text-center">
                     header
+                    <p className="text-gray-700">sdfdsfds</p>
                </div>
             </div>
         </div>
